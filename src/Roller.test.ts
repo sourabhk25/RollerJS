367bc7d293493f6632b8af12fc6dce9001e3d00a
sourabhk25 <sourabhkulkarni258@gmail.com>
2023-03-28 10:46:36 -0700

    Jest testcases added for Roller functions

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -7,6 +7,95 @@ describe("Smoke test", ()=> {
 })
 
 describe("Roller tests", ()=> {
-  test("Description", () => {
+  // test("Description", () => {
+  // });
+
+  test("Roller with 6 faces(Happy flow) constructor)", () => {
+    let myRoller: Roller = new Roller(6);
+    let actual: number = myRoller.getFaces();
+    expect(actual).toBe(6);
   });
+
+  test("Roller with 1 face(Sad flow) constructor)", () => {
+    let myRoller: Roller = new Roller(1);
+    let actual: number = myRoller.getFaces();
+    expect(actual).toBe(6);
+  });
+
+  test("Roller with 2 face(Sad flow) constructor)", () => {
+    let myRoller: Roller = new Roller(2);
+    let actual: number = myRoller.getFaces();
+    expect(actual).toBe(2);
+  });
+
+  //we keep 6 as default no. of faces for below testcases now, since we need to test roll function
+  test("Roll with 2 number (Happy flow) for roll function expect 2)", () => {
+    let myRoller: Roller = new Roller(6);
+    let actual: number = myRoller.roll(2);
+    expect(actual).toBe(2);
+  });
+
+  test("Roll with -1 number (Sad flow) for roll function expect 0)", () => {
+    let myRoller: Roller = new Roller(6);
+    let actual: number = myRoller.roll(-1);
+    expect(actual).toBe(0);
+  });
+
+  //for less no. of faces
+  test("Roll with 3 number (Sad flow) for roll function expect 0)", () => {
+    let myRoller: Roller = new Roller(2);
+    let actual: number = myRoller.roll(3);
+    expect(actual).toBe(0);
+  });
+
+  test("Roll with 10 number (Sad flow) for roll function expect 0)", () => {
+    let myRoller: Roller = new Roller(6);
+    let actual: number = myRoller.roll(10);
+    expect(actual).toBe(0);
+  });
+
+  test("Get last Roll with (Sad flow) for last function)", () => {
+    let myRoller: Roller = new Roller(6);
+    let actual: number = myRoller.last();
+    expect(actual).toBe(0); //since no roll has done
+  });
+
+  test("Get last Roll with (sad flow) for last function)", () => {
+    let myRoller: Roller = new Roller(6);
+    let temp: number = myRoller.roll(10);
+    let actual: number = myRoller.last();
+    expect(actual).toBe(0); //since wrong roll has done
+  });
+
+  test("Get last Roll with (Happy flow) for last function)", () => {
+    let myRoller: Roller = new Roller(6);
+    let temp: number = myRoller.roll(2);
+    let actual: number = myRoller.last();
+    expect(actual).toBe(2); //since wrong roll has done
+  });
+
+  test("Return a Map object with keys from 1 to the number of faces and values of 0 if no rolls have been made yet", () => {
+    let myRoller: Roller = new Roller(6);
+    const map = myRoller.distribution();
+    expect(map).toEqual(new Map([[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0]]));
+  });
+
+  test("Return a Map object with keys from 1 to the number of faces and values of no. of rolls after some valid rolls", () => {
+    let myRoller: Roller = new Roller(6);
+    let temp1: number = myRoller.roll(2);
+    let temp2: number = myRoller.roll(1);
+    const map = myRoller.distribution();
+    expect(map).toEqual(new Map([[1, 1], [2, 1], [3, 0], [4, 0], [5, 0], [6, 0]]));
+  });
+
+  // test("should return a Map object with the correct values for each face that has been rolled", () => {
+  //   die.roll(3);
+  //   expect(die.distribution()).toEqual(new Map([[1, 0], [2, 0], [3, 1], [4, 0], [5, 0], [6, 0]]));
+  //
+  //   die.roll(6);
+  //   expect(die.distribution()).toEqual(new Map([[1, 0], [2, 0], [3, 1], [4, 0], [5, 0], [6, 1]]));
+  // });
+
+
+
 });
--- src/Roller.ts
+++ src/Roller.ts
@@ -3,11 +3,23 @@ export class Roller {
     private _last: number;
     private _faces: number;
 
+
+    //getter for faces for testing constructor
+    getFaces(): number {
+        return this._faces;
+    }
+
     // specifies the number of faces the die has. It must have at least 2 faces
     // so any invalid values should default to 6 to represent the common 
     // six-sided die.
     constructor(faces: number) {
-        faces;
+        if (faces < 2) {
+            this._faces = 6;
+        } else {
+            this._faces = faces;
+        }
+        this._last = 0;
+        this._distribution = new Map<number, number>();
     }
 
     // specifies which number should be rolled and returns the recorded roll. 
@@ -20,14 +32,27 @@ export class Roller {
     //   3. the distribution should update the distribution record so that the
     //      key (value rolled) has an updated value that increases the number
     //      of rolls for that face by 1
-    roll(value: number): number{
+    roll(value: number): number {
+        if (value < 1 || value > this._faces) {
+            return 0;
+        }
+
+        this._last = value;
+
+        let temp: number = this._distribution.get(value);
+        if (temp) {
+            this._distribution.set(value, temp+1);
+        } else {
+            this._distribution.set(value, 1);
+        }
+
         return value;
     }
 
     // returns the value of the latest die roll. If no rolls have been made
     // yet, then it should return 0.
     last(): number {
-        return 0;
+        return this._last;
     }
 
     // returns a Map that represents the current distribution of rolls for each
@@ -35,7 +60,12 @@ export class Roller {
     // value and its value should be the number of times that face has been
     // rolled. Even if a face has not yet been rolled, its key should have a
     // value of 0.
-    distribution(): Map<number, number>{
-        return new Map<number, number>();
+    distribution(): Map<number, number> {
+        const map = new Map<number, number>();
+        for (let i = 1; i <= this._faces; i++) {
+            let temp: number = this._distribution.get(i);
+            map.set(i, temp || 0);
+        }
+        return map;
     }
 } // end of Roller class
